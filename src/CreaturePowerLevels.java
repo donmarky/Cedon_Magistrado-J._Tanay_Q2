66bdbc7485e0ed7ca95c5deb4a3d709503ea5dc4
Jonan Leonel <majonanleonel@my.cspc.edu.ph>
2024-12-14 21:10:53 +0800

    Selection Sort and Displaying the Array

--- src/CreaturePowerLevels.java
+++ src/CreaturePowerLevels.java
@@ -41,5 +41,28 @@ public class CreaturePowerLevels {
         }
     }
 
-    
+    // Selection sort method
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            // Find the maximum element in the unsorted array
+            int maxIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] > array[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+       // Swap the found maximum element with the first element
+            int temp = array[maxIndex];
+            array[maxIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+    // Method to display the array
+    public static void displayArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
 }
